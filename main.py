5b6290348dbcdf614484b960730e7122cd253104
Garima1518 <garima.roy@g-ushin.com>
2025-05-21 12:30:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+"""This module contains greeting functions."""  # <- module docstring
+
 def hello():
+    """Print a greeting."""
     print("hi")
 
 
 def bye():
+    """Print a farewell."""
     print("bye")
 
 
